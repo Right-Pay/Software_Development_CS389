424abe717dbe21f21ac8c1e5cf2afd56c801e5b3
Xander Hughes <xhughesey@comcast.net>
2023-10-09 18:44:00 -0400

    Changes

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,17 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors);
+
+const UserRouter = require('./routes/UserRouter');
 
 app.listen(port, () => {
     // tslint:disable-next-line:no-console
