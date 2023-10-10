7d21a94dc6cf33cd3e111c413e6265c44a87db33
tibesnoff <tibesnoff@gmail.com>
2023-10-10 13:13:36 -0400

    Generate now with tailwinds

--- app/tools/generate.js
+++ app/tools/generate.js
@@ -1,12 +1,11 @@
-/* eslint-disable quotes */
 const {generateTemplateFiles} = require('generate-template-files');
 
 generateTemplateFiles([
   {
-    option: 'Create a new component with an api',
+    option: 'Create a new component',
     defaultCase: '(pascalCase)',
     entry: {
-      folderPath: './tools/templates/componentAndAPI',
+      folderPath: './tools/templates/component',
     },
     stringReplacers: [
       {
@@ -17,11 +16,6 @@ generateTemplateFiles([
         question: 'What is the name of this component?',
         slot: '__name__',
       },
-      {
-        question: 'What is this method calling the api for?',
-        slot: '__fetching__',
-      },
-      '__method__',
     ],
     dynamicReplacers: [
       {
@@ -44,7 +38,7 @@ generateTemplateFiles([
       pathAndFileNameDefaultCase: '(pascalCase)',
     },
     onComplete: results => {
-      console.log(`results`, results);
+      console.log('results', results);
     },
   },
   {
@@ -75,45 +69,7 @@ generateTemplateFiles([
       pathAndFileNameDefaultCase: '(pascalCase)',
     },
     onComplete: results => {
-      console.log(`results`, results);
-    },
-  },
-  {
-    option: 'Create only an api',
-    defaultCase: '(pascalCase)',
-    entry: {
-      folderPath: './tools/templates/componentAndAPI/api',
-    },
-    stringReplacers: [
-      '__screen__',
-      {
-        question: 'What is this method calling the api for?',
-        slot: '__fetching__',
-      },
-      '__method__',
-    ],
-    dynamicReplacers: [
-      {
-        slot: '__src__',
-        slotValue: '',
-      },
-      {
-        slot: '/* eslint-disable @typescript-eslint/no-unused-vars */',
-        slotValue: '',
-      },
-      {
-        slot: '/* eslint-disable prettier/prettier */',
-        slotValue: '',
-      },
-      {slot: '/*', slotValue: ''},
-      {slot: '*/', slotValue: ''},
-    ],
-    output: {
-      path: './src/Components/Screens/__name__(pascalCase)',
-      pathAndFileNameDefaultCase: '(pascalCase)',
-    },
-    onComplete: results => {
-      console.log(`results`, results);
+      console.log('results', results);
     },
   },
 ]);
--- app/tools/templates/component/__name__.tsx
+++ app/tools/templates/component/__name__.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import type {PropsWithChildren} from 'react';
 import type {
-  SearchNavigationRoutesType,
+  /*__screen__NavigationRoutesType,*/
   NavigationRoutesType,
-} from '../../../types/NavigationRoutesType';
+} from '../../..__src__(camelCase)/types/NavigationRoutesType';
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
 import type {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
 import type {CompositeScreenProps} from '@react-navigation/native';
 import {styled} from 'nativewind';
 
-type SearchScreenProps = CompositeScreenProps<
-  NativeStackScreenProps<SearchNavigationRoutesType, 'SearchScreen'>,
+type __screen__ScreenProps = CompositeScreenProps<
+  NativeStackScreenProps<__screen__NavigationRoutesType, '__screen__Screen'>,
   BottomTabScreenProps<NavigationRoutesType>
 > &
   PropsWithChildren;
@@ -19,12 +19,12 @@ type SearchScreenProps = CompositeScreenProps<
 const StylizedView = styled(View);
 const StylizedText = styled(Text);
 
-const SearchUi: React.FC<SearchScreenProps> = () => {
+const __name__: React.FC<__screen__ScreenProps> = () => {
   return (
     <StylizedView className="flex-1 justify-center items-center text-center">
-      <StylizedText>This is the SearchUi component</StylizedText>
+      <StylizedText>This is the __name__ component</StylizedText>
     </StylizedView>
   );
 };
 
-export default SearchUi;
+export default __name__;
