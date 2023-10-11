5b01367343418b222c2d7f34f4ba28df2bec016e
Tibesnoff <Tibesnoff@gmail.com>
2023-10-11 19:21:35 -0400

    Removed style

--- app/src/Components/Screens/Home/Home.tsx
+++ app/src/Components/Screens/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import type {PropsWithChildren} from 'react';
 import type {
   HomeNavigationRoutesType,
