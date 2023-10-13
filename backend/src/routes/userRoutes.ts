be21fcb697f9b782d47ffbf54f3315a9269f48a5
xhughesey <xhughesey@comcast.net>
2023-10-12 20:35:13 -0400

    checkpoint

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -4,25 +4,55 @@ import { IJsonResponse } from '../types/jsonResponse';
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response) => {
+// Get User by ID
+router.get("/", checkJwt, (req: Request, res: Response) => {
+  const auth = req.auth;
+  const token = auth?.token; // The raw JWT token
   const response: IJsonResponse = {
-    message: "TLX API - Users",
-    success: true
-  };
-  res.json(response);
-});
-
-router.get("/:id", checkJwt, (req: Request, res: Response) => {
-  const response: IJsonResponse = {
-    message: "TLX API - User by ID " + req.params.id,
+    message: "TLX API - User by ID " + auth?.payload.sub,
     success: true,
-    data: req.auth
+    data: auth
   };
   res.json(response);
-  // const auth = req.auth;
-  // auth?.token; // The raw JWT token
-  // console.log(auth);
 });
 
+// Register User
+router.post("/", checkJwt, (req: Request, res: Response) => {
+  const auth = req.auth;
+  const token = auth?.token; // The raw JWT token
+  const data = req.body;
+  const response: IJsonResponse = {
+    message: "TLX API - Register User " + data.username + " " + auth?.payload.sub,
+    success: true,
+    data: auth
+  };
+  res.json(response);
+});
+
+// Update User
+router.put("/", checkJwt, (req: Request, res: Response) => {
+  const auth = req.auth;
+  const token = auth?.token; // The raw JWT token
+  const data = req.body;
+  const response: IJsonResponse = {
+    message: "TLX API - Update User " + data.username + " " + auth?.payload.sub,
+    success: true,
+    data: auth
+  };
+  res.json(response);
+});
+
+// Delete User
+router.delete("/", checkJwt, (req: Request, res: Response) => {
+  const auth = req.auth;
+  const token = auth?.token; // The raw JWT token
+  const data = req.body;
+  const response: IJsonResponse = {
+    message: "TLX API - Delete User " + data.username + " " + auth?.payload.sub,
+    success: true,
+    data: auth
+  };
+  res.json(response);
+});
 
 export default router;
