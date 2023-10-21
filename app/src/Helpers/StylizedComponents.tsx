24b7a5370fa90d68e1082cafb3e03d9cd8d51189
tibesnoff <tibesnoff@gmail.com>
2023-10-21 15:32:39 -0400

    Adding to stylized components

--- app/src/Helpers/StylizedComponents.tsx
+++ app/src/Helpers/StylizedComponents.tsx
@@ -2,14 +2,14 @@ import {styled} from 'nativewind';
 import {TextInput, Text, View, Pressable, FlatList} from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
 
-export const WrapperView = styled(View, 'flex-1 items-center');
+export const WrapperView = styled(View, 'flex-1 items-center bg-light-green');
 
 export const Title = styled(
   Text,
-  'mt-20 text-3xl text-center font-bold text-green-500',
+  'mt-20 text-3xl text-center font-bold text-dark-green',
 );
 
-export const Subtitle = styled(Text, 'text-xl text-center text-green-500');
+export const Subtitle = styled(Text, 'text-xl text-center text-dark-green');
 
 export const FinePrint = styled(Text, 'text-sm text-black');
 
@@ -17,12 +17,12 @@ export const FinePrintButton = styled(Pressable, 'flex pb-1');
 
 export const AuthButton = styled(
   Pressable,
-  'flex color items-center justify-center m-2 text-xl text-black flex h-9 w-5/12 rounded-xl bg-green-500 shadow-sm transition-colors',
+  'flex color items-center justify-center m-2 text-xl text-black flex h-9 w-5/12 rounded-xl bg-dark-green shadow-sm transition-colors',
 );
 
 export const AuthInputBox = styled(
   TextInput,
-  'px-3 py-1 m-1 text-xl text-black flex h-9 w-1/2 rounded-xl border border-green-500 bg-transparent shadow-sm transition-colors',
+  'px-3 py-1 m-1 text-xl text-black flex h-9 w-1/2 rounded-xl border border-dark-green bg-light-green bg-transparent shadow-sm transition-colors',
 );
 
 export const MainButton = styled(
@@ -30,7 +30,7 @@ export const MainButton = styled(
   'text-3xl p-2 border-2 border-gray-600 rounded-xl m-2',
 );
 
-export const ButtonText = styled(Text, 'text-xl');
+export const ButtonText = styled(Text, 'text-xl text-white');
 
 export const GoogleMapsView = styled(
   MapView,
