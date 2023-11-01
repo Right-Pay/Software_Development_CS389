6b9dc0359d302702deb2a3fba20f21ec82a6e327
tibesnoff <tibesnoff@gmail.com>
2023-11-01 14:52:57 -0400

    More z

--- app/src/Helpers/Dropdown.tsx
+++ app/src/Helpers/Dropdown.tsx
@@ -29,6 +29,7 @@ const DropdownComponent = (props: DropdownProps) => {
         setValue={setValue}
         setItems={setItems}
         placeholder={props.placeholder}
+        listItemContainerStyle={{zIndex: 100}}
         onChangeValue={event => handleSetValue(event?.toString() as string)}
       />
     </View>
